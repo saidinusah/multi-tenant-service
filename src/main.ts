db8f1394d4d247a1fef70d0f200d81fafab5cc2f
said <saidinusah29@gmail.com>
2025-03-20 22:24:50 +0000

    chore: subscriptions crud, and updated member creation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AppValidationPipe } from "./pipes/validation.pipe";
 
 async function bootstrap() {
   const app = (await NestFactory.create(AppModule)).setGlobalPrefix("/api");
-  // const envVars = process.env;
 
   app.useGlobalPipes(new AppValidationPipe());
   app.enableCors();
